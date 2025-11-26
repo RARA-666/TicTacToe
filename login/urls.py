4bfa53bfcba5cab2763c3856b1e4459dbe23f3b1
RaRaIsStrong <raraisstrong666@gmail.com>
2025-11-26 23:21:31 +0900

    시작
    이게 장고가 안되네

--- login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from login.views import IndexPage
-
-urlpatterns = [ 
-    path("", IndexPage.as_view()),
-]
--- main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from main.views import IndexPage
-
-urlpatterns = [ 
-    path("", IndexPage.as_view()),
-]
